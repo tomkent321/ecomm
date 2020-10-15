8563a19f939c564f0b8fee1cf7ce58866ac84b33
tomkent321 <tomkent321@yahoo.com>
2020-10-15 10:50:25 -0600

    Home Screen Products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,12 +4,22 @@ import { Card } from 'react-bootstrap'
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}></a>
-      <Card.Img src={product.image} variant='top' />
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </a>
+
       <Card.Body>
-        <Card.Title className='center'>
-          <h6>{product.name}</h6>
-        </Card.Title>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
