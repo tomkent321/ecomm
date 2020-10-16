ccfc89f503ca251aa76c4cf88e47ddd1ded5c34e
tomkent321 <tomkent321@yahoo.com>
2020-10-16 14:45:57 -0600

    set up backend

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
+// useEffect allows calling backend
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 import Product from '../components/Product'
 
-import products from '../products'
+// import products from '../products'
 
 const HomeScreen = () => {
+  //next statement uses the use state: [var, fuctionName] = useState(initial state)
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
