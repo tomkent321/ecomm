0e5a23307ea4322812dd4aaf06f1c726b6e331d3
tomkent321 <tomkent321@yahoo.com>
2020-10-18 10:40:44 -0600

    productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import products from './data/products.js'
+
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 //Then use .env files to store sensitive data
@@ -15,14 +17,10 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+//this 'mounts' this route into app to the correct router
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
