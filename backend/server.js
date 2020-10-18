edee04aafce9bf0f2ca554a385abe25a6d7807c3
tomkent321 <tomkent321@yahoo.com>
2020-10-17 22:19:25 -0600

    data schemas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+//Then use .env files to store sensitive data
+
+connectDB()
 
 const app = express()
 
@@ -20,4 +25,7 @@ app.get('/api/products/:id', (req, res) => {
 })
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
