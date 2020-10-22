58c490dc8e12d39546a1bd288d184cd125803fbe
tomkent321 <tomkent321@yahoo.com>
2020-10-22 16:09:03 -0600

    add controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+
+  res.json(products)
+})
+
+// @desc    Fetch one product
+// @route   GET /api/products
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    //if not set will default to status 500
+    res.status(404)
+    throw new Error('Product not found')
+    //uses the errorMiddleware errorHandler
+  }
+})
+
+export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,41 +1,8 @@
 import express from 'express'
-
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    // throw new Error('Some error')
-
-    res.json(products)
-  })
-)
-
-// @desc    Fetch one product
-// @route   GET /api/products/:id
-// @access  Public
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      //if not set will default to status 500
-      res.status(404)
-      throw new Error('Product not found')
-      //uses the errorMiddleware errorHandler
-    }
-  })
-)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
