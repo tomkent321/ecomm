bb9aab902da56fe01bae4653b8ecc9c75601ea87
Thomas Hurd <thomashurd@ThomasKMac.local>
2020-12-17 13:02:58 -0700

    more adjustments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen.js'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
@@ -59,4 +59,3 @@ const App = () => {
 }
 
 export default App
-
